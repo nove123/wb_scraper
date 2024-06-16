ff88ab654e4d61a56d8126e812e5e5e1db1607d6
nove <nove123hero@yandex.by>
2024-06-16 23:38:48 +0300

    Привел в порядок версию без асинхронного сбора данных

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import time
-from aiohttp import ClientSession
-import asyncio
-import json
 import requests
 import pandas as pd
-import openpyxl
-def make_url(shard, query):
-    return f"https://catalog.wb.ru/catalog/{shard}/v4/filters?appType=1&{query}&curr=rub&dest=-1257786&spp=30&uclusters=2"
 
-#В последней вложенности получаем все категории предметов
+
+# В последней вложенности собираем все категории предметов
 def get_last(data_list, url):
     headers = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     response = requests.get(url, headers=headers)
@@ -26,18 +21,15 @@ def get_last(data_list, url):
                         "nesting_lvl": nesting_lvl})
                 except:
                     continue
-        return data_list
     else:
         print("Ошибка при запросе к API")
 
-#проходимся по подкаталогам
+
 def get_subcatalog(data_list, data, nesting_lvl):
     for item in data:
         try:
             id_item = item["id"]
             name = item["name"]
-            shard = item["shard"]
-            query = item["query"]
             data_list.append({
                 "id_item": id_item,
                 "name": name,
@@ -45,34 +37,37 @@ def get_subcatalog(data_list, data, nesting_lvl):
             if "childs" in item:
                 get_subcatalog(data_list, item["childs"], nesting_lvl + 1)
             else:
-                url_last_subcatalog = f"https://catalog.wb.ru/catalog/{shard}/v4/filters?appType=1&{query}&curr=rub&dest=-1257786&spp=30&uclusters=2"
-                get_last(data_list, url_last_subcatalog)
+                if "shard" in item and "query" in item:
+                    shard = item["shard"]
+                    query = item["query"]
+                    url_last_subcatalog = f"https://catalog.wb.ru/catalog/{shard}/v4/filters?appType=1&{query}&curr" \
+                                          f"=rub&dest=-1257786&spp=30&uclusters=2 "
+                    get_last(data_list, url_last_subcatalog)
+                else:
+                    url_last_subcatalog = item["url"]
+                    get_last(data_list, url_last_subcatalog)
         except:
             continue
 
 
-def get_catalog(filename):
+def get_catalog():
     url = 'https://static-basket-01.wbbasket.ru/vol0/data/main-menu-ru-ru-v2.json'
     headers = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        with pd.ExcelWriter(f'{filename}.xlsx') as writer:
-            data = response.json()
-            for item in data[2:5]:
-                try:
-                    if "childs" in item:
-                        name = item["name"]
-                        data_list = []
-                        get_subcatalog(data_list, item["childs"], 1)
-                        #Сохраняем лист с одной главной категорией
-                        df = pd.DataFrame(data_list)
-                        df.to_excel(excel_writer=writer, sheet_name=name)
-                        print(f"Сохранили -- {name}")
-                except:
-                    continue
-        print(f'Все сохранено в {filename}.xlsx')
+        data = response.json()
+        res = dict()
+        for item in data:
+            try:
+                name = item["name"]
+                res[name] = []
+                get_subcatalog(res[name], item["childs"], 1)
+            except:
+                continue
+        return res
     else:
         print("Ошибка при запросе к API")
+
 
 def save_excel(data, filename):
     with pd.ExcelWriter(f'{filename}.xlsx') as writer:
@@ -81,10 +76,10 @@ def save_excel(data, filename):
             df.to_excel(excel_writer=writer, sheet_name=key)
     print(f'Все сохранено в {filename}.xlsx')
 
+
 def main():
-    print(time.strftime('%X'))
-    get_catalog("catalog")
-    print(time.strftime('%X'))
+    save_excel(get_catalog(), "catalog")
+
 
 if __name__ == "__main__":
     main()
